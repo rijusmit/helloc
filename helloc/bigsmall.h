5736fecfe281fdc91a4b87aed009c685fe2d03f3
Rijusmit Biswas <rijusmit.biswas@hotmail.com>
2017-11-19 01:49:35 +0530

    greatest no. done

--- helloc/bigsmall.h
+++ helloc/bigsmall.h
@@ -1,1 +1,78 @@
 #pragma once
+
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+
+void big();
+void ibig();
+
+void big()
+{
+	int x = 10;
+	int y = 6;
+	int z = 7;
+	//let me find out which one is the greatest number
+
+	if (x > y)
+	{
+		if (x > z)
+		{
+			printf("\n x is Greatest");
+		}
+		else
+		{
+			printf("\n z is Greatest");
+		}
+	}
+	else
+	{
+		if (y > z)
+		{
+			printf("\n y is Greatest");
+		}
+		else
+		{
+			printf("\n z is Greatest");
+		}
+	}
+
+}
+void ibig()
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	printf("\nEnter Value Of x:");
+	scanf_s("%d", &x);
+	printf("\nEnter Value Of y:");
+	scanf_s("%d", &y); 
+	printf("\nEnter Value Of z:");
+	scanf_s("%d", &z);
+	//let me find out which one is the greatest number
+
+	if (x > y)
+	{
+		if (x > z)
+		{
+			printf("\n x is Greatest");
+		}
+		else
+		{
+			printf("\n z is Greatest");
+		}
+	}
+	else
+	{
+		if (y > z)
+		{
+			printf("\n y is Greatest");
+		}
+		else
+		{
+			printf("\n z is Greatest");
+		}
+	}
+}
