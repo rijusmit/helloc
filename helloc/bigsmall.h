ebdc6e063d7abc16687721f19c35480a95727acd
Rijusmit Biswas <rijusmit.biswas@hotmail.com>
2017-11-26 18:29:46 +0530

    animation

--- helloc/bigsmall.h
+++ helloc/bigsmall.h
@@ -4,12 +4,26 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <conio.h>
+#include<windows.h>
+#include <time.h>
 
 void big();
 void ibig();
 
-
+void gotoxy(int x, int y);
 void count200();
+
+void gotoxy(int x, int y)
+{
+	COORD c = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+//
+//void wait(unsigned timeout)
+//{
+//	timeout += std::clock();
+//	while (std::clock() < timeout) continue;
+//}
 
 void big()
 {
@@ -81,8 +95,10 @@ void ibig()
 }
 
 
-void count200()
+void count200(int width,int height, int w, int h)
 {
+	
+
 	/*for (int i = 0; i <= 255; i++)
 	{
 		printf("\n [%d = %c ]",i, i);
@@ -95,27 +111,48 @@ void count200()
 	char eq = 205;
 	char pipe = 186;
 
+	//clrscr();
 
+	gotoxy(w, h);
+	
 
-	putchar(tl);
-	for (int i = 0; i < 20; i++)
+	printf("%c", tl);
+	//putchar(tl);
+	for (int i = w+1; i < (width + w) ; i++)
 	{
-		putchar(205);
+		gotoxy(i, h);
+		printf("%c", eq);
+
+		gotoxy(i, h + height);
+		printf("%c", eq);
+
 	}
 
+	gotoxy(width + w, h);
+	printf("%c",tr);
+	
+	
+	gotoxy(width + w, height +h );
+	//putchar(200);
+	printf("%c", bl);
 
-	putchar(187);
+	gotoxy(w , height + h );	
+	printf("%c", br);
 
-	printf("\n");
 
-	putchar(200);
-	for (int i = 0; i < 20; i++)
+	for (int i = h +1; i < height + h; i++)
 	{
-		putchar(205);
+		gotoxy(w, i);
+		//putchar(205);
+		printf("%c", pipe);
+
+		gotoxy(width + w, i);
+		//putchar(205);
+		printf("%c", pipe);
+
 	}
 
-	putchar(188);
-
+	
 
 
 }
