b24e5c48fa50467cff953fdd58b836f62c0cf359
rijusmit <rijusmit.biswas@gmail.com>
2022-01-16 12:38:50 +0530

    commit

--- y2022/test.cpp
+++ y2022/test.cpp
@@ -1,16 +1,34 @@
 #include<iostream>
+#include<string>
 #include<stdio.h>
 #include<conio.h>
-#include<string>
 
 using namespace std;
 
+int exp(int a,int c){
+    int b =1 ;
+    for (int i=1;i<=c;i++){
+        b *= a;
+    }
+    return b;
+}
+
+
 int main(){
-    int c=1;
-    while(c<40){
-        cout << c <<endl;
-        c++;
-    };
-    
-    return 0;
+/*int matrix[3][3] = {
+    {1,2,3},
+    {1,7,8},
+    {5,6,1}
+};
+//cout << "The 2nd Element of 2nd row is "<<matrix[1][1]<<" "<<exp(4,2);
+for (int i = 0;i<3;i++){
+    for (int j = 0;j<3;j++){
+        
+        cout << matrix[i][j] << ends;
+    }
+    cout << endl;
+}
+return 0;*/
+
+
 }
